87d9db7887dd1d6d793c2bc11e648792c5800094
Viktoriya1Lesheva <viktoriyalesheva88@gmail.com>
2025-04-10 04:44:47 +0000

    added fiolder class4

---
+++ Class4/varible.tf
@@ -0,0 +1,15 @@
+variable aws_key {
+    default = "hello-key"
+}
+
+variable region {
+    default = "us-east-2"
+}
+
+variable port1 {
+    default = 22
+}
+
+variable port2 {
+    default = 80
+}
